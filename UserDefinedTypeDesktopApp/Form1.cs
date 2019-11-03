b98c553617aaf07153b336d31b5e7b3e31b76f18
Ali hassan faruk Asif <asifkhondaker@yahoo.com>
2019-11-03 22:00:56 +0600

    Add project files.

---
+++ UserDefinedTypeDesktopApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserDefinedTypeDesktopApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        List<Person> persons = new List<Person>();
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Person aPerson = new Person();
+
+
+            aPerson.firstName = firstNameTextBox.Text;
+            aPerson.middleName = middleNameTextBox.Text;
+            aPerson.lastName = lastNameTextBox.Text;
+
+            firstNameTextBox.Text = " ";
+            middleNameTextBox.Text = " ";
+            lastNameTextBox.Text = " ";
+
+            persons.Add(aPerson);
+        }
+
+        private void ShowAllFullNamesButton_Click(object sender, EventArgs e)
+        {
+            fullNameListBox.Items.Clear();
+            foreach(Person person in persons)
+            {
+                fullNameListBox.Items.Add(person.GetFullName());
+            }
+        }
+    }
+}
---
+++ UserDefinedTypeDesktopApp/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedTypeDesktopApp
+{
+    class Person
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+
+
+        public string GetFullName()
+        {
+            string fullName = firstName + " " + middleName + " " + lastName;
+            return fullName;
+        }
+    }
+
+    
+}
